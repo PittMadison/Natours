425f71e23ba5117ebd5ca0e30e135014f3afd6a2
Pitt Madison <karimovpg@gmail.com>
2019-05-18 20:45:30 +0300

    media queries mixin created and applied to html (overall font-size)

--- js/index.js
+++ js/index.js
@@ -4,6 +4,5 @@ let body = document.querySelector('.preload');
 document.addEventListener('DOMContentLoaded', classRemove);
 
 function classRemove ()  {
-    body.classList.remove('preload');
+    body.classList.toggle('preload');
 };
-
